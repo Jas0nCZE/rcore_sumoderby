0266b7f425203fba011bb9f5f8cbb8b7fc77c968
Jas0nCZE <81179024+Jas0nCZE@users.noreply.github.com>
2021-08-21 11:42:41 +0200

    Create cs.lua

---
+++ [derby]/rcore_derby/locales/cs.lua
@@ -0,0 +1,14 @@
+Locales = {
+    ["points"] = "body",
+    ["start"] = '~r~POJĎME!',
+
+    ["1_place"] = "~y~První místo",
+    ["2_place"] = '~c~Druhý místo',
+    ["3_place"] = '~o~Třetí místo',
+
+    ["other_position"] = '~n~~w~%s Umístil na %i pozici s %i body',
+    ["others"] = '~g~Ostatní',
+    -- server
+    ["join"] =  "Hráč (%s) připojil arénu (%s) Volné místa: %i/%i",
+    ["leave"] =  "Hráč (%s) opustil arénu (%s) Volné místa: %i/%i",
+}
